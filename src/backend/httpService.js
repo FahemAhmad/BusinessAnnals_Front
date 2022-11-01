f6fc531f3b1dfe595cbd59c7b1d4d417ed0bcec9
Faheem Ahmad <faheemahmad0108@gmail.com>
2022-11-01 22:47:51 +0500

    link update

--- src/backend/httpService.js
+++ src/backend/httpService.js
@@ -4,7 +4,7 @@ import { getCookie } from "../Auth/auth";
 const authToken = getCookie("token");
 
 axios.defaults.baseURL =
-  "http://ec2-46-137-193-76.ap-southeast-1.compute.amazonaws.com:5000";
+  "http://ec2-13-212-216-200.ap-southeast-1.compute.amazonaws.com:5000";
 axios.defaults.headers.common["Authorization"] = `Bearer ${authToken}`;
 
 axios.interceptors.response.use(null, (error) => {
